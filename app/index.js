103871ae48a692b6a3281cf6570938ffe3ce50e7
manudevops726 <amanasa726@gmail.com>
2025-05-26 15:45:35 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ const port = 80;
 app.get('/', (req, res) => {
   res.send('Hello from Node.js app running on custom AMI! by using packer-jenkins by Mr.Veerababu sir 2025');
 });
-####veera
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
