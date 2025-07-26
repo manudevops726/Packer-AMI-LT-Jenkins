9f80fb33db115c3a8ee00a3ffbb111b89b42ea71
manudevops726 <amanasa726@gmail.com>
2025-07-26 23:56:13 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app running on custom AMI! by using packer-jenkins by Mr.Veerababu sir 2025');
+  res.send('Hello from Node.js app running on custom AMI! without custom ami create launch template by using packer-jenkins by Mr.Veerababu sir 2025');
 });
 
 app.listen(port, () => {
